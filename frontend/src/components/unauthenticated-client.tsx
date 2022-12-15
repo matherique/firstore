da4b9b06cd12f98d97c0b51a37204af96ef3e33b
Matheus Henrique <matherique@gmail.com>
2022-12-15 20:45:35 -0300

    fix: image component

--- frontend/src/components/unauthenticated-client.tsx
+++ frontend/src/components/unauthenticated-client.tsx
@@ -12,7 +12,7 @@ export default function UnauthenticatedClient({ children }: Props) {
       <div className="container mx-auto px-6 py-12 h-full">
         <div className="flex justify-center items-center flex-wrap h-full g-6 text-slate-800">
           <div className="md:w-8/12 lg:w-4/12 mb-12 md:mb-0">
-            <Image src={Logo} layout="responsive" alt="logo" />
+            <Image src={Logo} alt="logo" />
           </div>
           <div className="md:w-2/12 lg:w-3/12 lg:ml-20">{children}</div>
         </div>
