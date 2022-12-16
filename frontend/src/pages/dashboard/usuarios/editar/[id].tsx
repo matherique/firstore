2129b9920154e19eb324161f9fad9b05b68b926a
Matheus Henrique <matherique@gmail.com>
2022-12-15 21:23:28 -0300

    fix: update user title page

--- frontend/src/pages/dashboard/usuarios/editar/[id].tsx
+++ frontend/src/pages/dashboard/usuarios/editar/[id].tsx
@@ -81,7 +81,7 @@ const EditarUsuario: NextPage = () => {
 
   return <DashboardLayout>
     <header className="flex gap-2 justify-between mb-5">
-      <h1 className="text-3xl text-slate-500 font-bold">Cadastrar Usuários</h1>
+      <h1 className="text-3xl text-slate-500 font-bold">Editar Usuários</h1>
       <Link
         href="/dashboard/usuarios/listar"
         className="px-7 py-3 bg-slate-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-slate-800 hover:shadow-lg focus:bg-slate-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-slate-800 active:shadow-lg transition duration-150 ease-in-out"
